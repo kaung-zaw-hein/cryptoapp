5eee276adcbf4539eec66b7f22491a4858512119
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-05-29 20:59:27 +0630

    cryptocurrencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Footer } from './components';
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails} from './pages';
 import './App.css'
 
@@ -25,19 +25,7 @@ const App = () => (
           </Routes>
         </div>
       </Layout>
-      <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
-          <Link to="/">
-            Cryptoverse Inc.
-          </Link> <br />
-          All Rights Reserved.
-        </Typography.Title>
-        <Space>
-          <Link to="/">Home</Link>
-          <Link to="/exchanges">Exchanges</Link>
-          <Link to="/news">News</Link>
-        </Space>
-      </div>
+     <Footer/>
     </div>
   </div>
 );
